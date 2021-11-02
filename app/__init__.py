c5fc4061bc02591287ca50d368163cb8df11bb87
Audrey Andoy <audreylandoy@gmail.com>
2021-11-01 21:14:13 -0700

    added os access to environment variable

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASDE_URI")
     app.config['SQLALCHEMY_ECHO'] = True 
 
     # Import models here
